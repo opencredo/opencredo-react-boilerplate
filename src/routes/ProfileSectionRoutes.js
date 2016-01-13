de43d709feda6c1d42d1e029a5bd6c8daf9460b6
Vincent Martinez <eykosioux@gmail.com>
2016-01-13 15:12:04 +0000

    Remove unused routes

--- src/routes/ProfileSectionRoutes.js
+++ src/routes/ProfileSectionRoutes.js
@@ -6,11 +6,9 @@ import ProfileLayout from 'layouts/ProfileLayout/ProfileLayout';
 
 // Pages
 import ProfileEditPage from 'pages/ProfileEditPage/ProfileEditPage';
-import MySpacesPage from 'pages/MySpacesPage/MySpacesPage';
 
 export default (
   <Route name="profile" path="/profile" component={ProfileLayout}>
     <IndexRoute component={ProfileEditPage} />
-    <Route name="my-spaces" path="spaces" component={MySpacesPage} />
   </Route>
 );
