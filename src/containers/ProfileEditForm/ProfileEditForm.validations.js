870c21457c03216ca0ff0013c709dc4eb32d8dab
Matt Calthrop <matt.calthrop@opencredo.com>
2016-01-29 11:14:00 +0000

    Fix bug: need complete list of validation fields

--- src/containers/ProfileEditForm/ProfileEditForm.validations.js
+++ src/containers/ProfileEditForm/ProfileEditForm.validations.js
@@ -16,4 +16,20 @@ export default {
     email: true,
     validateOnBlur: true,
   },
+  emailVerified: {
+    required: true,
+    validateOnBlur: true,
+  },
+  gender: {
+    required: true,
+    validateOnBlur: true,
+  },
+  locale: {
+    required: true,
+    validateOnBlur: true,
+  },
+  notes: {
+    required: false,
+    validateOnBlur: true,
+  },
 };
